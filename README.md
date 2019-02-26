5daaebc5ff328ebfe5f53adbb0d0db3063a8c9e8
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-02-25 16:18:55 -0800

    First

---
+++ README.md
@@ -0,0 +1,33 @@
+XPCS
+===============================
+
+version number: 
+author: Ron Pandolfi
+
+Overview
+--------
+
+XPCS GUI interface
+
+Installation / Usage
+--------------------
+
+To install use pip:
+
+    $ pip install XPCS
+
+
+Or clone the repo:
+
+    $ git clone 
+    $ python setup.py install
+    
+Contributing
+------------
+
+TBD
+
+Example
+-------
+
+TBD
---
+++ xicam/XPCS/__init__.py
@@ -0,0 +1,85 @@
+from qtpy.QtCore import *
+from qtpy.QtGui import *
+from qtpy.QtWidgets import *
+from qtpy import uic
+import pyqtgraph as pg
+
+from xicam.core import msg
+from xicam.plugins import GUIPlugin, GUILayout, ProcessingPlugin
+from xicam.plugins import manager as pluginmanager
+from xicam.core.execution import Workflow
+from xicam.gui.widgets.tabview import TabView
+from xicam.SAXS.widgets.SAXSViewerPlugin import SAXSViewerPlugin
+from xicam.core.data import NonDBHeader
+from xicam.gui.widgets.imageviewmixins import PolygonROI
+from pyqtgraph.parametertree import ParameterTree, Parameter
+
+
+class TwoTimeProcess(ProcessingPlugin):
+    ...
+
+
+class XPCSWorkflow(Workflow):
+    ...
+
+
+class OneTime(XPCSWorkflow):
+    ...
+
+
+class TwoTime(XPCSWorkflow):
+    ...
+
+
+class XPCSViewerPlugin(PolygonROI, SAXSViewerPlugin):
+    pass
+
+
+class XPCSProcessor(ParameterTree):
+    def __init__(self, *args, **kwargs):
+        super(XPCSProcessor, self).__init__()
+
+        self.param = Parameter(children=[{'name': 'Algorithm',
+                                          'type': 'list',
+                                          'values': {'1-time correlation': OneTime,
+                                                     '2-time correlation': TwoTime},
+                                          'value': '2-time correlation'}])
+
+
+class XPCS(GUIPlugin):
+    name = 'XPCS'
+
+    def __init__(self):
+        # Data model
+        self.headermodel = QStandardItemModel()
+        self.selectionmodel = QItemSelectionModel(self.headermodel)
+
+        # Widgets
+        self.calibrationsettings = pluginmanager.getPluginByName('DeviceProfiles', 'SettingsPlugin').plugin_object
+
+        # Toolbar
+        self.toolbar = QToolBar()
+
+        # Setup TabViews
+        self.rawtabview = TabView(self.headermodel,
+                                  widgetcls=XPCSViewerPlugin,
+                                  selectionmodel=self.selectionmodel,
+                                  bindings=[(self.calibrationsettings.sigGeometryChanged, 'setGeometry')],
+                                  geometry=self.getAI)
+
+        self.stages = {'XPCS': GUILayout(self.rawtabview,
+                                         right=self.calibrationsettings.widget,
+                                         top=self.toolbar)}
+
+        super(XPCS, self).__init__()
+
+    def appendHeader(self, header: NonDBHeader, **kwargs):
+        item = QStandardItem(header.startdoc.get('sample_name', '????'))
+        item.header = header
+        self.headermodel.appendRow(item)
+        self.selectionmodel.setCurrentIndex(self.headermodel.index(self.headermodel.rowCount() - 1, 0),
+                                            QItemSelectionModel.Rows)
+        self.headermodel.dataChanged.emit(QModelIndex(), QModelIndex())
+
+    def getAI(self):
+        return None
