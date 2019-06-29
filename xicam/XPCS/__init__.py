872eaa5928b5791fce770ac36b823e76f6ed475d
Ian Humphrey <ian.r.humphrey@gmail.com>
2019-06-28 17:07:33 -0700

    Separate XPCS algorithm stages

--- xicam/XPCS/__init__.py
+++ xicam/XPCS/__init__.py
@@ -4,6 +4,8 @@ from qtpy.QtWidgets import *
 from qtpy import uic
 import pyqtgraph as pg
 
+from matplotlib import pyplot as plt
+
 from xicam.core import msg
 from xicam.plugins import GUIPlugin, GUILayout, ProcessingPlugin
 from xicam.plugins import manager as pluginmanager
@@ -33,14 +35,75 @@ class XPCSViewerPlugin(PolygonROI, SAXSViewerPluginBase):
 class XPCSProcessor(ParameterTree):
     def __init__(self, *args, **kwargs):
         super(XPCSProcessor, self).__init__()
+        self._name = 'Algorithm'
+        self._type = 'list'
+        self._values = {}
+        self._value = ''
 
-        self.param = Parameter(children=[{'name': 'Algorithm',
+        # self.param = Parameter(children=[{'name': self._name,
+        #                                   'type': self._type,
+        #                                   'values': self._values,
+        #                                   'value': self._value}], name='Processor')
+        #
+        # self.setParameters(self.param, showTop=False)
+
+
+class OneTimeProcessor(XPCSProcessor):
+    def __init__(self, *args, **kwargs):
+        super(OneTimeProcessor, self).__init__()
+        for k, v in OneTimeAlgorithms.categories().items():
+            self._values[k] = v
+        self._value = OneTimeAlgorithms.default()
+        self.param = Parameter(children=[{'name': self._name,
                                           'type': 'list',
-                                          'values': {OneTime.name: OneTime,
-                                                     TwoTime.name: TwoTime,
-                                                     FourierAutocorrelator.name: FourierAutocorrelator},
-                                          'value': FourierAutocorrelator.name}], name='Processor')
+                                          'values': self._values,
+                                          'value': self._value}], name='1-Time Processor')
         self.setParameters(self.param, showTop=False)
+
+
+class TwoTimeProcessor(XPCSProcessor):
+    def __init__(self, *args, **kwargs):
+        super(TwoTimeProcessor, self).__init__()
+        for k, v in TwoTimeAlgorithms.categories().items():
+            self._values[k] = v
+        self._value = TwoTimeAlgorithms.default()
+        self.param = Parameter(children=[{'name': self._name,
+                                          'type': self._type,
+                                          'values': self._values,
+                                          'value': self._value}], name='2-Time Processor')
+        self.setParameters(self.param, showTop=False)
+
+
+class ProcessingCategories:
+    @staticmethod
+    def categories():
+        return {
+            TwoTimeAlgorithms.name: TwoTimeAlgorithms.categories(),
+            OneTimeAlgorithms.name: OneTimeAlgorithms.categories()
+        }
+
+
+class TwoTimeAlgorithms(ProcessingCategories):
+    name = '2-Time Algorithms'
+    @staticmethod
+    def categories():
+        return {TwoTime.name: TwoTime}
+
+    @staticmethod
+    def default():
+        return TwoTime.name
+
+
+class OneTimeAlgorithms(ProcessingCategories):
+    name = '1-Time Algorithms'
+    @staticmethod
+    def categories():
+        return {OneTime.name: OneTime,
+                FourierAutocorrelator.name: FourierAutocorrelator}
+
+    @staticmethod
+    def default():
+        return OneTime.name
 
 
 class CorrelationView(QWidget):
@@ -49,26 +112,37 @@ class CorrelationView(QWidget):
 
     This could be generalized into a ComboPlotView / PlotView / ComboView ...
     """
-    def __init__(self):
+    def __init__(self, model):
         super(CorrelationView, self).__init__()
         self.resultslist = QComboBox()
 
-        self.correlationplot = pg.PlotWidget()
+        self.plot = pg.PlotWidget()
 
-        self.model = self.resultslist.model() # type: QStandardItemModel
+        self.model = model # type: QStandardItemModel
+        self.resultslist.setModel(self.model)
         self.selectionmodel = self.resultslist.view().selectionModel()
 
         layout = QVBoxLayout()
         layout.addWidget(self.resultslist)
-        layout.addWidget(self.correlationplot)
+        layout.addWidget(self.plot)
         self.setLayout(layout)
 
         self.selectionmodel.currentChanged.connect(self.updatePlot)
 
+    # TODO TEMP method
+    def getResults(self, current):
+        return self.model.itemFromIndex(current).payload['result']['g2'].value.squeeze()
+
     def updatePlot(self, current, previous):
-        self.correlationplot.clear()
-        self.correlationplot.plot(
-            self.model.itemFromIndex(current).payload['result']['g2'].value.squeeze())
+        self.plot.clear()
+        self.plot.plot(self.getResults(current))
+        # # TODO temp
+        # from matplotlib import pyplot as plt
+        # plt.figure('current results')
+        # plt.plot(self.getResults(current))
+        # plt.xscale('log')
+        # plt.show()
+        # # TODO end
         self.resultslist.setCurrentIndex(current.row()) # why doesn't model/view do this for us?
 
     def appendData(self, data):
@@ -79,6 +153,31 @@ class CorrelationView(QWidget):
         self.selectionmodel.setCurrentIndex(
             self.model.index(self.model.rowCount() - 1, 0), QItemSelectionModel.Rows)
         self.model.dataChanged.emit(QModelIndex(), QModelIndex())
+
+    def setLabels(self, left, bottom):
+        self.plot.getPlotItem().setLabel('left', left)
+        self.plot.getPlotItem().setLabel('bottom', bottom)
+
+
+class OneTimeView(CorrelationView):
+
+    def __init__(self):
+        self.model = QStandardItemModel()
+        super(OneTimeView, self).__init__(self.model)
+        plotItem = self.plot.getPlotItem()
+        plotItem.setLabel('left', 'g<sub>2</sub>(&tau;)', 's')
+        plotItem.setLabel('bottom', '&tau;', 's')
+        plotItem.setLogMode(x=True)
+
+
+class TwoTimeView(CorrelationView):
+
+    def __init__(self):
+        self.model = QStandardItemModel()
+        super(TwoTimeView, self).__init__(self.model)
+        plotItem = self.plot.getPlotItem()
+        plotItem.setLabel('left', 't<sub>2</sub>', 's')
+        plotItem.setLabel('bottom', 't<sub>1</sub>', 's')
 
 
 class FileSelectionView(QWidget):
@@ -124,14 +223,14 @@ class FileSelectionView(QWidget):
             lambda current, _: self.correlationname.setPlaceholderText(current.data())
         )
 
-    def updateListView(self, start, end):
-        pass
-
 
 class XPCS(GUIPlugin):
     name = 'XPCS'
 
     def __init__(self):
+
+        self.resultsmodel = QStandardItemModel()
+
         # Data model
         self.headermodel = QStandardItemModel()
         self.selectionmodel = QItemSelectionModel(self.headermodel)
@@ -152,19 +251,29 @@ class XPCS(GUIPlugin):
                                   geometry=self.getAI)
 
         # Setup correlation view
-        self.correlationview = CorrelationView()
-        self.fileselectionview = FileSelectionView(self.headermodel, self.selectionmodel)
-        self.processor = XPCSProcessor()
+        # self.correlationview = CorrelationView()
+        self.twotimeview = TwoTimeView()
+        self.onetimeview = OneTimeView()
+        self.onetimefileselection = FileSelectionView(self.headermodel, self.selectionmodel)
+        self.twotimefileselection = FileSelectionView(self.headermodel, self.selectionmodel)
+        self.onetimeprocessor = OneTimeProcessor()
+        self.twotimeprocessor = TwoTimeProcessor()
+        # self.processor = XPCSProcessor()
         self.placeholder = QLabel('correlation parameters')
 
         self.stages = {'Raw(change)': GUILayout(self.rawtabview,
-                                         right=self.calibrationsettings.widget,
-                                         top=self.toolbar),
-                       'Correlate': GUILayout(self.correlationview,
-                                              top=self.toolbar,
-                                              right=self.fileselectionview,
-                                              rightbottom=self.processor,
-                                              bottom=self.placeholder)
+                                                # top=self.toolbar,
+                                                right=self.calibrationsettings.widget),
+                       '2-Time Correlation': GUILayout(self.twotimeview,
+                                                       top=self.toolbar,
+                                                       right=self.twotimefileselection,
+                                                       rightbottom=self.twotimeprocessor,
+                                                       bottom=self.placeholder),
+                       '1-Time Correlation': GUILayout(self.onetimeview,
+                                                       top=self.toolbar,
+                                                       right=self.onetimefileselection,
+                                                       rightbottom=self.onetimeprocessor,
+                                                       bottom=self.placeholder)
                        }
 
         # TODO -- should CorrelationDocument be a member?
@@ -185,11 +294,6 @@ class XPCS(GUIPlugin):
 
     def currentheader(self):
         return self.headermodel.itemFromIndex(self.selectionmodel.currentIndex()).header
-        # if self.selectionmodel.selectedIndexes() == 1:
-        #     return [self.headermodel.itemFromIndex(self.selectionmodel.currentIndex()).header]
-        # else:
-        #     model_indices = self.selectionmodel.selectedIndexes()
-        #     return [self.headermodel.itemFromIndex(i).header for i in model_indices]
 
     def currentheaders(self):
         selected_indices = self.selectionmodel.selectedIndexes()
@@ -198,27 +302,73 @@ class XPCS(GUIPlugin):
             headers.append(self.headermodel.itemFromIndex(model_index).header)
         return headers
 
-    def process(self):
-        workflow = self.processor.param['Algorithm']()
+    def addResults(self, data):
+        item = QStandardItem(data['name'])
+        item.payload = data
+        # Do not add if 'name' already in the model TODO temp
+        resultsmodel = self.currentModel()
+        resultsmodel.appendRow(item)
+        self.currentSelectionModel().setCurrentIndex(
+            resultsmodel.index(resultsmodel.rowCount() - 1, 0), QItemSelectionModel.Rows)
+        # self.selectionmodel.setCurrentIndex(
+        #     self.model.index(self.model.rowCount() - 1, 0), QItemSelectionModel.Rows)
+        resultsmodel.dataChanged.emit(QModelIndex(), QModelIndex())
 
-        data = [header.meta_array() for header in self.currentheaders()]
-        labels = [self.rawtabview.currentWidget().poly_mask()] * len(data)
-        workflow.execute_all(None,
-                             data=data,
-                             labels=labels,
-                             callback_slot=self.show_g2)
+    # TODO better way to do this (sig, slot)
+    def currentProcessor(self):
+        processor = self.stage['rightbottom']
+        if isinstance(processor, XPCSProcessor):
+            return processor
+        else:
+            return None
+
+    def currentSelectionModel(self):
+        if isinstance(self.stage['center'], CorrelationView):
+            return self.stage['center'].selectionmodel
+        else:
+            return None
+
+    def currentFileSelectionView(self):
+        if isinstance(self.stage['right'], FileSelectionView):
+            return self.stage['right']
+        else:
+            return None
+
+    def currentModel(self):
+        if isinstance(self.stage['center'], CorrelationView):
+            return self.stage['center'].model
+        else:
+            return None
+
+    # def currentSelectionModel(self):
+    #     model = self.currentModel()
+    #     if model:
+    #         return
+
+    def process(self):
+        # This should always pass, since this is the action for a process toolbar QAction
+        if self.currentProcessor():
+            workflow = self.currentProcessor().param['Algorithm']()
+
+            data = [header.meta_array() for header in self.currentheaders()]
+            labels = [self.rawtabview.currentWidget().poly_mask()] * len(data)
+            workflow.execute_all(None,
+                                 data=data,
+                                 labels=labels,
+                                 callback_slot=self.show_g2)
 
     def show_g2(self, result):
         data = {} # TODO -- temp { 'name': Uniqueresultname, 'result': result
-        if not self.fileselectionview.correlationname.displayText():
-            data['name'] = self.fileselectionview.correlationname.placeholderText()
+        fileselectionview = self.currentFileSelectionView()
+        if not fileselectionview.correlationname.displayText():
+            data['name'] = fileselectionview.correlationname.placeholderText()
         else:
-            data['name'] = self.fileselectionview.correlationname.displayText()
+            data['name'] = fileselectionview.correlationname.displayText()
 
         data['result'] = result
 
         try:
-            self.correlationview.appendData(data)
+            self.addResults(data)
             # if only a single image was selected, the value is 0 length.
             # self.correlationview.plotwidget.plot(result['g2'].value.squeeze())
 
