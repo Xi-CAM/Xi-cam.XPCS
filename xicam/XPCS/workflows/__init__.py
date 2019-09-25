36ea9e507218893d8f5fde1c6ed0481a133c2e2b
Ian Humphrey <ian.r.humphrey@gmail.com>
2019-09-25 15:44:35 -0700

    Move doc creation to XPCSWorkflow classes

--- xicam/XPCS/workflows/__init__.py
+++ xicam/XPCS/workflows/__init__.py
@@ -1,3 +1,7 @@
+import time
+
+import event_model
+
 from xicam.core.execution import Workflow
 from xicam.plugins import ProcessingPlugin
 
@@ -18,6 +22,66 @@ class TwoTime(XPCSWorkflow):
         super(TwoTime, self).__init__()
         self.addProcess(TwoTimeCorrelation())
 
+    @staticmethod
+    def document(**kwargs):
+        results = kwargs.get('results')
+        roi = kwargs.get('roi')
+        workflow = kwargs.get('workflow_pickle')
+
+        timestamp = time.time()
+
+        run_bundle = event_model.compose_run()
+        yield 'start', run_bundle.start_doc
+
+        source = 'Xi-cam'
+
+        peek_result = results[0]
+        g2_shape = peek_result['g2'].value.shape[0]
+        tau_shape = peek_result['lag_steps'].value.shape[0]
+        workflow = []
+        workflow_shape = len(workflow)
+
+        reduced_data_keys = {
+            'norm-0-g2': {'source': source, 'dtype': 'number', 'shape': [g2_shape]},
+            'tau': {'source': source, 'dtype': 'number', 'shape': [tau_shape]},
+            'dqlist': {'source': source, 'dtype': 'string', 'shape': []},  # todo -- shape
+            'workflow': {'source': source, 'dtype': 'string', 'shape': [workflow_shape]}
+        }
+        reduced_stream_name = 'reduced'
+        reduced_stream_bundle = run_bundle.compose_descriptor(data_keys=reduced_data_keys,
+                                                              name=reduced_stream_name)
+        yield 'descriptor', reduced_stream_bundle.descriptor_doc
+
+        # todo -- peek frame shape
+        frame_data_keys = {'frame': {'source': source, 'dtype': 'number', 'shape': []}}
+        frame_stream_name = 'primary'
+        frame_stream_bundle = run_bundle.compose_descriptor(data_keys=frame_data_keys,
+                                                            name=frame_stream_name)
+        yield 'descriptor', frame_stream_bundle.descriptor_doc
+
+        # todo -- store only paths? store the image data itself (memory...)
+        # frames = header.startdoc['paths']
+        frames = []
+        for frame in frames:
+            yield 'event', frame_stream_bundle.compose_event(
+                data={frame},
+                timestamps={timestamp}
+            )
+
+        for result in results:
+            yield 'event', reduced_stream_bundle.compose_event(
+                data={'norm-0-g2': result['g2'].value,
+                      'tau': result['lag_steps'].value,
+                      'dqlist': roi,
+                      'workflow': workflow},
+                timestamps={'norm-0-g2': timestamp,
+                            'tau': timestamp,
+                            'dqlist': timestamp,
+                            'workflow': workflow}
+            )
+
+        yield 'stop', run_bundle.compose_stop()
+
 
 class OneTime(XPCSWorkflow):
     name = '1-Time Correlation'
@@ -29,6 +93,75 @@ class OneTime(XPCSWorkflow):
         fitting = FitScatteringFactor()
         self.addProcess(fitting)
         self.autoConnectAll()
+
+    @staticmethod
+    def document(**kwargs):
+        results = kwargs.get('results')
+        roi = kwargs.get('roi')
+        workflow = kwargs.get('workflow_pickle')
+
+        timestamp = time.time()
+
+        run_bundle = event_model.compose_run()
+        yield 'start', run_bundle.start_doc
+
+        source = 'Xi-cam'
+
+        peek_result = results[0]
+        g2_shape = peek_result['g2'].value.shape[0]
+        import numpy as np
+        g2_err = np.zeros(g2_shape)
+        g2_err_shape = g2_shape
+        tau_shape = peek_result['lag_steps'].value.shape[0]
+        workflow = []
+        workflow_shape = len(workflow)
+
+        reduced_data_keys = {
+            'norm-0-g2': {'source': source, 'dtype': 'number', 'shape': [g2_shape]},
+            'norm-0-stderr': {'source': source, 'dtype': 'number', 'shape': [g2_err_shape]},
+            'tau': {'source': source, 'dtype': 'number', 'shape': [tau_shape]},
+            'g2avgFIT1': {'source': source, 'dtype': 'number', 'shape': [tau_shape]},
+            'dqlist': {'source': source, 'dtype': 'string', 'shape': []},  # todo -- shape
+            'workflow': {'source': source, 'dtype': 'string', 'shape': [workflow_shape]}
+        }
+        reduced_stream_name = 'reduced'
+        reduced_stream_bundle = run_bundle.compose_descriptor(data_keys=reduced_data_keys,
+                                                              name=reduced_stream_name)
+        yield 'descriptor', reduced_stream_bundle.descriptor_doc
+
+        # todo -- peek frame shape
+        frame_data_keys = {'frame': {'source': source, 'dtype': 'number', 'shape': []}}
+        frame_stream_name = 'primary'
+        frame_stream_bundle = run_bundle.compose_descriptor(data_keys=frame_data_keys,
+                                                            name=frame_stream_name)
+        yield 'descriptor', frame_stream_bundle.descriptor_doc
+
+        # todo -- store only paths? store the image data itself (memory...)
+        # frames = header.startdoc['paths']
+        frames = []
+        for frame in frames:
+            yield 'event', frame_stream_bundle.compose_event(
+                data={frame},
+                timestamps={timestamp}
+            )
+
+        for result in results:
+            yield 'event', reduced_stream_bundle.compose_event(
+                data={'norm-0-g2': result['g2'].value,
+                      'norm-0-stderr': g2_err,
+                      'tau': result['lag_steps'].value,
+                      'g2avgFIT1': result['fit_curve'].value,
+                      'dqlist': roi,
+                      'workflow': workflow},
+                timestamps={'norm-0-g2': timestamp,
+                            'norm-0-stderr': timestamp,
+                            'tau': timestamp,
+                            'g2avgFIT1': timestamp,
+                            'dqlist': timestamp,
+                            'workflow': workflow}
+            )
+
+        yield 'stop', run_bundle.compose_stop()
 
 
 class FourierAutocorrelator(XPCSWorkflow):
