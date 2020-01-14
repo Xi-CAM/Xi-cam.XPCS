3df1617d883727cf2308cd382431cd8660d814da
Ian Humphrey <ihumphrey@lbl.gov>
2020-01-14 11:14:44 -0800

    use find_namespace_package

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from codecs import open
 from os import path
 
-__version__ = '0.1.0'
+__version__ = '1.1.0'
 
 here = path.abspath(path.dirname(__file__))
 
